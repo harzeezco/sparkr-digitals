85d41c91607342fe4f6be556b9cd9b12340ee76a
harzeezco <wakilabdulazeez@gmail.com>
2024-05-13 08:48:23 +0700

    feat: implement our staff section and also done with layout

---
+++ src/pages/about-page/our-staff.tsx
@@ -0,0 +1,99 @@
+import AnimatedLink from '@/components/animated-text';
+import { cn } from '@/lib/cn';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { bricolage } from '../../../app/font';
+
+const VISION = [
+  {
+    src: 'staff1.webp',
+    title: 'Quadri Ismail',
+    desc: 'Co-founder & CEO',
+  },
+  {
+    src: 'staff2.webp',
+    title: 'Farihan Sodiq',
+    desc: 'Operation Manager',
+  },
+  {
+    src: 'staff3.webp',
+    title: 'Quadri Ismail',
+    desc: 'Co-founder & CEO',
+  },
+  {
+    src: 'staff4.webp',
+    title: 'Farihan Sodiq',
+    desc: 'Operation Manager',
+  },
+  {
+    src: 'staff1.webp',
+    title: 'Quadri Ismail',
+    desc: 'Co-founder & CEO',
+  },
+  {
+    src: 'staff2.webp',
+    title: 'Farihan Sodiq',
+    desc: 'Operation Manager',
+  },
+  {
+    src: 'staff3.webp',
+    title: 'Quadri Ismail',
+    desc: 'Co-founder & CEO',
+  },
+  {
+    src: 'staff4.webp',
+    title: 'Farihan Sodiq',
+    desc: 'Operation Manager',
+  },
+];
+
+const OurStaff = () => (
+  <section className='container mt-24 flex flex-col items-center'>
+    <h1
+      className={cn(
+        'mx-auto max-w-4xl text-center text-3xl font-light sm:text-4xl lg:text-5xl',
+        bricolage.className,
+      )}
+    >
+      Our talented and diverse team works together for one purpose—to
+      unlock the potential of those around us.
+    </h1>
+
+    <Link className='mt-6 font-medium' href='/case-study'>
+      <AnimatedLink
+        className='transition duration-200 hover:text-green-500'
+        title='JOIN OUR TEAM'
+      />
+      <hr className='border border-[#121718]/70' />
+    </Link>
+
+    <div className='mt-16 grid max-w-6xl grid-cols-4 gap-7'>
+      {VISION.map(({ desc, src, title }) => (
+        <div className='relative w-full cursor-pointer overflow-hidden transition-all duration-500'>
+          <Image
+            alt={title}
+            className='rounded-[12px] transition-all duration-300 hover:scale-105'
+            height={300}
+            src={`/images/${src}`}
+            width={280}
+          />
+
+          <div className='flex-col'>
+            <h2
+              className={cn(
+                'mt-4 font-bricolage text-2xl font-semibold',
+                bricolage.className,
+              )}
+            >
+              {title}
+            </h2>
+            <p className='text-lg opacity-70'>{desc}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  </section>
+);
+
+export default OurStaff;
